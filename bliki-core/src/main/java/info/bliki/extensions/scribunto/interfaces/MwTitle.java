58cee80d4d53542798389c383e220d05f36b227e
Jan Berkel <jan.berkel@gmail.com>
2014-04-13 15:01:07 +0200

    MwTitle fixes

--- bliki-core/src/main/java/info/bliki/extensions/scribunto/interfaces/MwTitle.java
+++ bliki-core/src/main/java/info/bliki/extensions/scribunto/interfaces/MwTitle.java
@@ -4,7 +4,11 @@ import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.LibFunction;
+import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
+
+import static info.bliki.wiki.namespaces.INamespace.NamespaceCode.MAIN_NAMESPACE_KEY;
+import static info.bliki.wiki.namespaces.INamespace.NamespaceCode.MEDIA_NAMESPACE_KEY;
 
 public class MwTitle extends MwInterface {
     @Override
@@ -17,18 +21,65 @@ public class MwTitle extends MwInterface {
         LuaTable table = new LuaTable();
         table.set("newTitle", newTitle());
         table.set("makeTitle", makeTitle());
-        table.set("getUrl", defaultFunction());
-        table.set("getContent", defaultFunction());
-        table.set("fileExists", defaultFunction());
+        table.set("getUrl", getUrl());
+        table.set("getContent", getContent());
+        table.set("fileExists", fileExists());
         table.set("protectionLevels", defaultFunction());
         return table;
     }
 
+    private LuaValue getUrl() {
+
+        return new LibFunction() {
+            /**
+             *  $text, $which, $query = null, $proto = null
+             */
+            @Override public Varargs invoke(Varargs args) {
+                return NIL;
+            }
+        };
+    }
+
+    private LuaValue fileExists() {
+        return new OneArgFunction() {
+            /**
+             * @param page
+             * @return Whether the file exists. For File- and Media-namespace titles, this is
+             * expensive. It will also be recorded as an image usage for File- and Media-namespace titles.
+             */
+            @Override public LuaValue call(LuaValue page) {
+                return NIL;
+            }
+        };
+    }
+
+    private LuaValue getContent() {
+        return new OneArgFunction() {
+            /**
+             * @param page
+             * @return the (unparsed) content of the page, or nil if there is no page.
+             * The page will be recorded as a transclusion.
+             */
+            @Override public LuaValue call(LuaValue page) {
+                return NIL;
+            }
+        };
+    }
+
     private LuaValue newTitle() {
         return new TwoArgFunction() {
+            /**
+             * Handler for title.new
+             *
+             * @param text_or_id       string|int Title or page_id to fetch
+             * @param defaultNamespace string|int Namespace name or number to use if $text_or_id doesn't override
+             * @return array Lua data
+             */
             @Override
             public LuaValue call(LuaValue text_or_id, LuaValue defaultNamespace) {
-                return title(defaultNamespace, text_or_id,
+
+                return title(defaultNamespace,
+                        text_or_id,
                         LuaValue.valueOf("fragment"),
                         LuaValue.valueOf("interwiki"));
             }
@@ -36,10 +87,19 @@ public class MwTitle extends MwInterface {
     }
 
     private LuaValue makeTitle() {
+        /**
+         *
+         * Creates a title object with title title in namespace namespace, optionally with the
+         * specified fragment and interwiki prefix. namespace may be any key found in mw.site.namespaces.
+         * If the resulting title is not valid, returns nil.
+         * @param $ns           string|int Namespace
+         * @param $text         string Title text
+         * @param $fragment     string URI fragment
+         * @param $interwiki    string Interwiki code
+         */
         return new LibFunction() {
             @Override
             public Varargs invoke(Varargs args) {
-                // ns, title, fragment, interwiki
                 LuaValue ns    = args.arg(1);
                 LuaValue title = args.arg(2);
                 LuaValue fragment = args.arg(3);
@@ -53,7 +113,7 @@ public class MwTitle extends MwInterface {
     public LuaValue getSetupOptions() {
         LuaTable table = new LuaTable();
         table.set("thisTitle", title());
-        table.set("NS_MEDIA", "NS_MEDIA");
+        table.set("NS_MEDIA", MEDIA_NAMESPACE_KEY.code);
         return table;
     }
 
@@ -71,7 +131,7 @@ public class MwTitle extends MwInterface {
         table.set("isRedirect", "");
         table.set("subjectNsText", "");
         table.set("interwiki", interwiki);
-        table.set("namespace", ns);
+        table.set("namespace", LuaValue.valueOf(MAIN_NAMESPACE_KEY.code));
         table.set("nsText", "");
         table.set("text", "");
         table.set("id", title);
