f0b5412a4afebcf2af921b91112d9cfd2b1e3c78
Jan Berkel <jan@berkel.fr>
2016-10-06 12:25:10 +0200

    Add options for mw.html

--- bliki-core/src/main/java/info/bliki/extensions/scribunto/engine/lua/interfaces/MwHtml.java
+++ bliki-core/src/main/java/info/bliki/extensions/scribunto/engine/lua/interfaces/MwHtml.java
@@ -4,6 +4,26 @@ import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
 
 public class MwHtml implements MwInterface {
+    /**
+     * string Prefix and suffix for temporary replacement strings
+     * for the multipass parser.
+     *
+     * \x7f should never appear in input as it's disallowed in XML.
+     * Using it at the front also gives us a little extra robustness
+     * since it shouldn't match when butted up against identifier-like
+     * string constructs.
+     *
+     * Must not consist of all title characters, or else it will change
+     * the behavior of <nowiki> in a link.
+     *
+     * Must have a character that needs escaping in attributes, otherwise
+     * someone could put a strip marker in an attribute, to get around
+     * escaping quote marks, and break out of the attribute. Thus we add
+     * `'".
+     */
+    private static final String MARKER_SUFFIX = "-QINU`\"'\u007f";
+	private static final String MARKER_PREFIX = "\u007f'\"`UNIQ-";
+
     @Override
     public String name() {
         return "mw.html";
@@ -11,12 +31,14 @@ public class MwHtml implements MwInterface {
 
     @Override
     public LuaTable getInterface() {
-        LuaTable iface = new LuaTable();
-        return iface;
+        return new LuaTable();
     }
 
     @Override
     public LuaValue getSetupOptions() {
-        return null;
+        LuaTable opts = new LuaTable();
+        opts.set("uniqPrefix", MARKER_PREFIX);
+        opts.set("uniqSuffix", MARKER_SUFFIX);
+        return opts;
     }
 }
