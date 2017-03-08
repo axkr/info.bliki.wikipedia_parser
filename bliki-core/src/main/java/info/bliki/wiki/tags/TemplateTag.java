982d41aa8f45303a8fb548b7f1c11d97725015e7
Thomas Mortagne <thomas.mortagne@gmail.com>
2017-03-08 16:34:17 +0100

    Issue #43 Introduce option in the parser to generate a tag for a template call

---
+++ bliki-core/src/main/java/info/bliki/wiki/tags/TemplateTag.java
@@ -0,0 +1,31 @@
+package info.bliki.wiki.tags;
+
+import java.io.IOException;
+
+import info.bliki.wiki.filter.ITextConverter;
+import info.bliki.wiki.model.IWikiModel;
+
+/**
+ * Wiki tag to indicate a template call.
+ * <p>
+ * TODO: add support for #renderHTML.
+ */
+public class TemplateTag extends HTMLTag
+{
+    public TemplateTag(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public void renderHTML(ITextConverter converter, Appendable writer, IWikiModel model) throws IOException
+    {
+        // TODO: not supported yet
+    }
+
+    @Override
+    public boolean isReduceTokenStack()
+    {
+        return false;
+    }
+}
